def2cc7ccaefa02b0399821276f8388924e14e7c
berserker1984 <laerte.mancio@gmail.com>
2024-04-21 10:58:17 -0300

    Layout conteudo step3 finalizado

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 
-const acionarCheckbox = () =>{
+
+  
+ 
+const checkBoxStep3 = ()=>{
+    const conteudoCheckBoxPreco = document.getElementsByClassName('conteudo-step-3');
+    const checkBoxPreco = document.getElementsByClassName('checkbox-addons');
+
+    
+    for (let i=0; i<checkBoxPreco.length; i++) {
+        if (checkBoxPreco[i].checked) {
+            conteudoCheckBoxPreco[i].style.backgroundColor = 'hsl(217, 100%, 97%)';
+            conteudoCheckBoxPreco[i].style.border = '1px solid  hsl(213, 96%, 18%)';
+
+           
+          } else {
+            conteudoCheckBoxPreco[i].style.backgroundColor = 'hsl(0, 0%, 100%)';
+            conteudoCheckBoxPreco[i].style.border = '1px solid hsl(229, 24%, 87%)';
+          }
+    }
+}
+
+
+  const acionarCheckbox = () =>{
     
     const slider = document.getElementById('controle-slider')
     const posicaoAtual = window.getComputedStyle(slider).justifyContent;
@@ -7,9 +29,14 @@ const acionarCheckbox = () =>{
     const elementoYearly = document.getElementsByClassName('label-yearly');
     const descontoPlanos = document.getElementsByClassName('desconto');
     const precoPlanos = document.getElementsByClassName('preco');
+    
+    
+    
 
     const monthly = elementoMonthly[0]
     const yearly = elementoYearly[0]
+
+    
 
     if (posicaoAtual === 'start') {
         slider.style.justifyContent = 'end'        
@@ -23,6 +50,9 @@ const acionarCheckbox = () =>{
         precoPlanos[0].innerHTML = '$90/yr'
         precoPlanos[1].innerHTML = '$120/yr'
         precoPlanos[2].innerHTML = '$150/yr'
+        precoPlanos[3].innerHTML = '$10/yr'
+        precoPlanos[4].innerHTML = '$20/yr'
+        precoPlanos[5].innerHTML = '$20/yr'
         
         
     } else if (posicaoAtual === 'end'){
@@ -37,6 +67,9 @@ const acionarCheckbox = () =>{
         precoPlanos[0].innerHTML = '$9/yr'
         precoPlanos[1].innerHTML = '$12/yr'
         precoPlanos[2].innerHTML = '$15/yr'
+        precoPlanos[3].innerHTML = '$1/yr'
+        precoPlanos[4].innerHTML = '$2/yr'
+        precoPlanos[5].innerHTML = '$2/yr'
         
         
     }
